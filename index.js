a045a005b66889da28f8903312598a9726c00b84
Amanda Maree Jones <amandamareejones@gmail.com>
2024-08-09 21:51:56 +1000

    API call test 1

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = "09te212a1c952dfb01916b44eoad6c23";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#weather-app-city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
