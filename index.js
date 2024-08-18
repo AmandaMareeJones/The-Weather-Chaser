58d01aad2506c2ac3cb8964f6f278d2c5e55e504
Amanda Maree Jones <amandamareejones@gmail.com>
2024-08-18 11:16:21 +1000

    JS - added forecast function

--- index.js
+++ index.js
@@ -51,7 +51,31 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml += `   
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-emoji">⛅</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>13°C </strong>
+          </div>
+          <div class="weather-forecast-temperature">4°C</div>
+        </div>
+      </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHtml = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Brisbane");
+displayForecast();
